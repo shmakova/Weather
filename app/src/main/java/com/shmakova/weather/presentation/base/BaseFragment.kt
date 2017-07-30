82e5a44083e8df6bc67f05716548455bd04b31d8
shmakova <shmakova-nastya@yandex.ru>
2017-07-31 00:29:32 +0300

    Update versions

---
+++ app/src/main/java/com/shmakova/weather/presentation/base/BaseFragment.kt
@@ -0,0 +1,6 @@
+package com.shmakova.weather.presentation.base
+
+import android.support.v4.app.Fragment
+
+open class BaseFragment : Fragment() {
+}
---
+++ app/src/main/java/com/shmakova/weather/presentation/weather/WeatherFragment.kt
@@ -0,0 +1,11 @@
+package com.shmakova.weather.presentation.weather
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.shmakova.weather.presentation.base.BaseFragment
+
+class WeatherFragment : BaseFragment() {
+}
